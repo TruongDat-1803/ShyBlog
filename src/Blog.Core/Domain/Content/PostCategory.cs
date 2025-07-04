dc523fec7ba4e4866958a813d0989c7c4ff005e4
TruongDat-1803 <truongdat1803@gmail.com>
2025-07-04 17:42:08 +0700

    Create Entities

---
+++ src/Blog.Core/Domain/Content/PostCategory.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Core.Domain.Content
+{
+    [Table("PostCategories")]
+    [Index(nameof(Slug), IsUnique = true)]
+    public class PostCategory
+    {
+        [Key] public Guid Id { get; set; }
+        [MaxLength(200)]
+        public required string Name { get; set; }
+        [Column(TypeName = "varchar(200)")]
+        public required string Slug { get; set; }
+        [Required]
+        public Guid? ParentId { get; set; }
+        [MaxLength(500)]
+        public bool IsActive { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateModified { get; set; }
+
+        [MaxLength(160)]
+        public string? SeoDescription { get; set; }
+        public int SortOrder { get; set; }
+    }
+}
---
+++ src/Blog.Core/Domain/Content/Series.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Core.Domain.Content
+{
+    [Table("Series")]
+    [Index(nameof(Slug), IsUnique = true)]
+    public class Series
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [MaxLength(200)]
+        [Required]
+        public required string Name { get; set; }
+        [MaxLength(200)]
+        public string? Description { get; set; }
+        [Column(TypeName = "varchar(200)")]
+        public required string Slug { get; set; }
+        public bool IsActive { get; set; }
+        public int SortOrder { get; set; }
+        [MaxLength(200)]
+        public string? SeoDescription { get; set; }
+        [MaxLength(200)]
+        public string? Thumbnail { get; set; }
+        public string? Content { get; set; }
+        public Guid OwnerUserId { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
